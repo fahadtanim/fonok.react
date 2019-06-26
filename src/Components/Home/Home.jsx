6cd124304803f2fa7d8c327a71742e080b2aa7ec
Fahad Tanim <fahadtanim@gmail.com>
2019-06-27 02:27:01 +0600

    Updates

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import HomeSlider from './HomeSlider/HomeSlider';
+import HomeServices from './HomeServices/HomeServices';
+import "./Home.css";
+import HomeAbout from './HomeAbout/HomeAbout';
+import HomeTestimonial from './HomeTestimonial/HomeTestimonial';
+import HomeContact from './HomeContact/HomeContact';
+class Home extends Component {
+    state = {  }
+    componentDidMount(){
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+          });
+    }
+    componentWillMount(){
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+          });
+    }
+    render() { 
+        return ( 
+            <React.Fragment>
+                <HomeSlider></HomeSlider>
+                <div className="row" id = "get-down-row"></div>
+                <HomeServices></HomeServices>
+                <HomeAbout></HomeAbout>
+                <HomeTestimonial></HomeTestimonial>
+                <HomeContact></HomeContact>
+            </React.Fragment>
+            );
+    }
+}
+
+ 
+export default Home;
---
+++ src/Components/Home/HomeAbout/HomeAbout.jsx
@@ -0,0 +1,137 @@
+import React, { Component } from "react";
+import "./HomeAbout.css";
+
+class HomeAbout extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div className="row" style={{backgroundImage : "url('./assets/images/bg-about-me.jpg')", backgroundSize:"cover", backgroundPosition: "center center"}}>
+          <div className="container">
+            <div className="row home-about-parts-container valign-wrapper">
+              <div className="col s12 m12 l12 content-center">
+                <div className="row">
+                  <p className="home-about-greetings primary-red-color">
+                    Nice to meet you
+                  </p>
+                </div>
+                <div className="row">
+                  <span className="primary-white-color">
+                    <p className="home-about-name-dummy primary-white-color">
+                      My Name Is
+                    </p>
+                    <p className="home-about-name-main primary-white-color">
+                      Md. Iftekhar Uddin Emon
+                    </p>
+                  </span>
+                </div>
+                <div className="row">
+                  <p className="home-about-designation">
+                    Founder of Fonok's Photobox
+                  </p>
+                </div>
+                <div className="row">
+                  <div className="col l6 m6 s12">
+                    <div className="row">
+                      <p className="home-about-details">
+                        I was born in the UK and raised in the African desert by
+                        bushmen. Okay, that might be a stretch – my parents were
+                        from the UK, but I did grow up shaking Kalahari sand out
+                        of my shoes.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col l6 m6 s12">
+                    <div className="row valign-wrapper">
+                      <img
+                        src="./assets/images/lens-hood.png"
+                        alt="camera-icon.png"
+                        className="home-about-icon content-center"
+                      />
+                      <span className="home-about-icon-counter primary-red-color">
+                        132
+                      </span>
+                      <span className="col s5 home-about-icon-details content-center ">
+                        Pounds of Equipment
+                      </span>
+                    </div>
+                  </div>
+                  <div className="col l6 m6 s12">
+                    <div className="row valign-wrapper">
+                      <img
+                        src="./assets/images/camera-icon.png"
+                        alt="camera-icon.png"
+                        className="home-about-icon content-center"
+                      />
+                      <span className="home-about-icon-counter primary-red-color">
+                        280
+                      </span>
+                      <span className="col s5 home-about-icon-details content-center ">
+                        Finished Photosessions
+                      </span>
+                    </div>
+                  </div>
+                  <div className="col l6 m6 s12">
+                    <div className="row valign-wrapper">
+                      <img
+                        src="./assets/images/photographer-umbrella.png"
+                        alt="camera-icon.png"
+                        className="home-about-icon content-center"
+                      />
+                      <span className="home-about-icon-counter primary-red-color">
+                        195
+                      </span>
+                      <span className="col s5 home-about-icon-details content-center ">
+                        Studio Sessions
+                      </span>
+                    </div>
+                  </div>
+                  <div className="col l6 m6 s12">
+                    <div className="row valign-wrapper">
+                      <img
+                        src="./assets/images/camera-focus.png"
+                        alt="camera-icon.png"
+                        className="home-about-icon content-center"
+                      />
+                      <span className="home-about-icon-counter primary-red-color">
+                        253
+                      </span>
+                      <span className="col s5 home-about-icon-details content-center ">
+                        Happy Clients
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                
+                <div className="row home-about-signature">
+                  <div className="col s1 content-center">
+                    <div className="row">
+                      <p className="home-about-signature-title">
+                        Best
+                        <br />
+                        Regards
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col s4 content-center">
+                    <div className="row">
+                      <img
+                        src="./assets/images/signature.png"
+                        alt="Signature"
+                        className="home-about-signature-image"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default HomeAbout;
