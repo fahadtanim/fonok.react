3494156fb409c7669288e2d0abd59bd2b909ff5a
Md. Fahad-Uz-Zaman Khan <fahad.khan@dsinnovators.com>
2019-06-27 15:24:35 +0600

    first deploy

---
+++ precache-manifest.5cce655863d6ff2c000a0e7e08096ffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435863bbad0486698c59696345ce89ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bed1dc39055774bd7393",
+    "url": "/static/css/main.91a90d6a.chunk.css"
+  },
+  {
+    "revision": "541581b1dc977830730c",
+    "url": "/static/js/2.78f23aa4.chunk.js"
+  },
+  {
+    "revision": "bed1dc39055774bd7393",
+    "url": "/static/js/main.f6a306f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
